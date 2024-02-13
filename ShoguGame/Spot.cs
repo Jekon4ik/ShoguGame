feefc2ab3015ab0940ed74d8eebacd6679ae96d7
Jekon4ik <ghfswhrh@gmail.com>
2024-02-13 18:47:28 +0100

    spot class implementation

---
+++ ShoguGame/Spot.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoguGame
+{
+    internal class Spot
+    {
+        private int x;
+        private int y;
+        private Piece piece;
+
+        public Spot(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+            piece = null;
+        }
+        public Spot(int x, int y, Piece piece)
+        {
+            this.x = x;
+            this.y = y;
+            this.piece = piece;
+        }
+        public void setX(int x) { this.x = x;}
+        public int getX() { return x; }
+        public void setY(int y) { this.y = y; }
+        public int getY() { return y; }
+        public void setPiece(Piece piece) {  this.piece = piece; }
+        public Piece getPiece() {  return piece; }
+
+    }
+}
