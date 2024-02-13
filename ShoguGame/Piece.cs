1706825650502d3f99682053727a43464d279aa1
Jekon4ik <ghfswhrh@gmail.com>
2024-02-13 18:55:10 +0100

    implementation of Piece class

---
+++ ShoguGame/Piece.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoguGame
+{
+    public class Piece
+    {
+        private PieceType pieceType;
+        private bool isWhite;
+        private bool isAlive;
+
+        public void setPieceType(PieceType pieceType) { this.pieceType = pieceType; }
+        public PieceType getPiece() { return pieceType; }
+        public bool IsWhite
+        {
+            get { return isWhite; }
+            set { isWhite = value; }
+        }
+        public bool IsAlive
+        {
+            get { return isAlive; }
+            set {isAlive = value; }
+        }
+    }
+}
